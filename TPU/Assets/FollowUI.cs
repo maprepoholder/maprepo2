d995417758e0e09c1006daa7bd7b387949d75c33
alla <anyway.amy@gmail.com>
2019-02-06 16:18:24 +0300

    1

---
+++ TPU/Assets/FollowUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FollowUI : MonoBehaviour
+{
+    public Camera cam;
+    public TextMeshProUGUI textMesh;
+    public Transform positionText;
+
+
+    [HideInInspector]
+    public Transform thisTransforn;
+
+    public int currentCount;
+    Vector3 positionDisable = new Vector3(2000,2000,-2000);
+
+    private string _text;
+    public string Text
+    {
+        get { return _text; }
+        set
+        {
+            if (textMesh) textMesh.text = value;
+            _text = value;
+            Debug.LogError(_text +" - " +gameObject.name);
+            StartCoroutine(WaiteAnHide());
+            positionText.SetAsLastSibling();
+        }
+        
+    }
+
+    private IEnumerator WaiteAnHide()
+    {
+        if(_text == "Hide") yield  break;
+
+        yield return new WaitForSeconds(10);
+        Close();
+    }
+
+    public void Close()
+    {
+        StopAllCoroutines();
+        thisTransforn.position = positionDisable;
+        Text = "Hide";
+
+        var myItemCopy = ControlText.Instance.Listfollow[currentCount - 1];
+        ControlText.Instance.Listfollow.Remove(ControlText.Instance.Listfollow[currentCount - 1]);
+        ControlText.Instance.Listfollow.Insert(ControlText.Instance.Listfollow.Count, myItemCopy);
+    }
+
+    void Awake()
+    {
+        thisTransforn = transform;
+        thisTransforn.position = positionDisable;
+    }
+	void LateUpdate ()
+	{
+	    if (positionText/* && thisTransforn.position != positionDisable*/)
+	    {
+	        Vector3 pos = cam.WorldToScreenPoint(this.transform.position);
+	        positionText.position = pos;
+	    }
+	}
+}
