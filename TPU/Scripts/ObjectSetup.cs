7a446e155e5618d1bd24a1a64231071f6ebccdb8
alla <anyway.amy@gmail.com>
2019-02-06 12:35:55 +0300

    9

---
+++ TPU/Scripts/ObjectSetup.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum LAYERS_Mat
+{
+    mat_layer_Car_traffic,                      //Джидение автотранспорта
+    mat_layer_Pedestrian_traffic,               //Джидение пешеходов
+    mat_layer_NGPT_traffic,               //Джидение пешеходов
+
+
+    mat_layer_UDS,                              //УДС
+    mat_layer_Transplant_Terminal,              //Терминал пересадки
+
+    mat_layer_Administrative_building,          //Административное здание
+    mat_layer_Underground_pedestrian_crossing,  //Подземный пешеходный переход
+    mat_layer_Elevated_pedestrian_crossing,     //Надземный пешеходный переход
+    mat_layer_Pedestrian_canopy,                //Пешеходный навес
+
+    mat_layer_Interceptive_parking,             //Перехватывающий паркинг
+    mat_layer_Parking,                         //Парковка
+
+    mat_layer_Reversal_playground,              //Отстойно-разворотная площадка
+    mat_layer_Stopping_NGPT,                    //Остановка НГПТ
+    mat_layer_Station_NGPT,                    //Станция НГПТ
+    
+
+    mat_layer_Shopping_center,                 //Торговый центр
+    mat_layer_Apartments,                      //Апартаменты
+    mat_layer_Offices,                         //Офисы
+    mat_layer_Residential_development,         //Жилая застойка
+
+    mat_layer_Athletic_wellness_complex,       //Физкультурно - оздоровительный комплекс 
+    mat_layer_AZS,                             //АЗС
+    mat_layer_Entertaining_center,             //Развлекательно-досуговый центр
+    mat_layer_Cinema,                          //Кинотеатр
+    mat_layer_Showroom,                        //Выставочный зал
+    mat_layer_Production_facility,             //Производственный объект
+    mat_layer_Kindergarten,                    //Десткий сад
+    mat_layer_School,                          //Школа
+    mat_layer_Polyclinic,                      //Поликлиника
+    mat_layer_Social_object,                   //Социальный объект
+    mat_layer_subway,                          //Метрополитен
+
+
+}
+public enum LAYERS_Name
+{
+    Движение_автотранспорта,                      //Джидение автотранспорта
+    Движение_пешеходов,               //Джидение пешеходов
+    НГПТ,               //Джидение пешеходов
+    Станция_ЖД,
+
+    УДС,                              //УДС
+    Терминал_пересадки,              //Терминал пересадки
+
+    Административное_здание,          //Административное здание
+    Подземный_пешеходный_переход,  //Подземный пешеходный переход
+    Надземный_пешеходный_переход,     //Надземный пешеходный переход
+    Пешеходный_навес,                //Пешеходный навес
+
+    Перехватывающий_паркинг,             //Перехватывающий паркинг
+    Парковки,                         //Парковка
+
+    Отстойно_D_разворотная_площадка,              //Отстойно-разворотная площадка
+    Остановка_НГПТ,                    //Остановка НГПТ
+    Станция_НГПТ,                    //Станция НГПТ
+
+
+    Торговый_центр,                 //Торговый центр
+    Апартаменты,                      //Апартаменты
+    Офисы,                         //Офисы
+    Жилая_застойка,         //Жилая застойка
+
+    Физкультурно_D_оздоровительный_комплекс,       //Физкультурно - оздоровительный комплекс 
+    АЗС,                             //АЗС
+    Развлекательно_D_досуговый_центр,             //Развлекательно-досуговый центр
+    Кинотеатр,                          //Кинотеатр
+    Выставочный_зал,                        //Выставочный зал
+    Производственный_объект,             //Производственный объект
+    Десткий_сад,                    //Десткий сад
+    Школа,                          //Школа
+    Поликлиника,                      //Поликлиника
+    Социальный_объект,                   //Социальный объект
+    Метрополитен                                    //Метрополитен 
+}
+
+public struct SettingLayer
+{
+    public static Dictionary<LAYERS_Name, string> LayerNames = new Dictionary<LAYERS_Name, string>()
+    {
+        {LAYERS_Name.Движение_автотранспорта, "mat_layer_Car_traffic"},
+        {LAYERS_Name.Движение_пешеходов,"mat_layer_Pedestrian_traffic"},
+        {LAYERS_Name.НГПТ,"mat_layer_NGPT_traffic"},
+
+        {LAYERS_Name.УДС,"mat_layer_UDS"},
+        {LAYERS_Name.Терминал_пересадки,"mat_layer_Transplant_Terminal"},
+        {LAYERS_Name.Административное_здание,"mat_layer_Administrative_building"},
+        {LAYERS_Name.Подземный_пешеходный_переход,"mat_layer_Underground_pedestrian_crossing"},
+        {LAYERS_Name.Остановка_НГПТ,"mat_layer_Stopping_NGPT"},
+        {LAYERS_Name.Пешеходный_навес,"mat_layer_Pedestrian_canopy"},
+        {LAYERS_Name.Перехватывающий_паркинг,"mat_layer_Interceptive_parking"},
+        {LAYERS_Name.Отстойно_D_разворотная_площадка,"mat_layer_Reversal_playground"},
+        {LAYERS_Name.Надземный_пешеходный_переход,"mat_layer_Elevated_pedestrian_crossing"},
+        {LAYERS_Name.Станция_НГПТ,"mat_layer_Station_NGPT"},
+        {LAYERS_Name.Парковки,"mat_layer_Parking"},
+        {LAYERS_Name.Торговый_центр,"mat_layer_Shopping_center"},
+        {LAYERS_Name.Апартаменты,"mat_layer_Apartments"},
+        {LAYERS_Name.Офисы,"mat_layer_Offices"},
+        {LAYERS_Name.Жилая_застойка,"mat_layer_Residential_development"},
+    
+        {LAYERS_Name.Физкультурно_D_оздоровительный_комплекс,"mat_layer_Athletic_wellness_complex"},
+        {LAYERS_Name.АЗС,"mat_layer_AZS"},
+        {LAYERS_Name.Развлекательно_D_досуговый_центр,"mat_layer_Entertaining_center"},
+        {LAYERS_Name.Кинотеатр,"mat_layer_Cinema"},
+        {LAYERS_Name.Выставочный_зал,"mat_layer_Showroom"},
+        {LAYERS_Name.Производственный_объект,"mat_layer_Production_facility"},
+        {LAYERS_Name.Десткий_сад,"mat_layer_Kindergarten"},
+        {LAYERS_Name.Школа,"mat_layer_School"},
+        {LAYERS_Name.Поликлиника,"mat_layer_Polyclinic"},
+        {LAYERS_Name.Социальный_объект,"mat_layer_Social_object"},
+
+        {LAYERS_Name.Метрополитен,"mat_layer_subway"},
+        {LAYERS_Name.Станция_ЖД ,"mat_layer_Railway_station"}
+    };
+    public static Dictionary<LAYERS_Mat, LAYERS_Name> LayerMat = new Dictionary<LAYERS_Mat, LAYERS_Name>()
+    {
+        {LAYERS_Mat.mat_layer_Car_traffic,LAYERS_Name.Движение_автотранспорта },
+        {LAYERS_Mat.mat_layer_Pedestrian_traffic,LAYERS_Name.Движение_пешеходов},
+
+        {LAYERS_Mat.mat_layer_UDS,LAYERS_Name.УДС},
+        {LAYERS_Mat.mat_layer_Transplant_Terminal,LAYERS_Name.Терминал_пересадки},
+        {LAYERS_Mat.mat_layer_Administrative_building,LAYERS_Name.Административное_здание},
+        {LAYERS_Mat.mat_layer_Underground_pedestrian_crossing,LAYERS_Name.Подземный_пешеходный_переход},
+        {LAYERS_Mat.mat_layer_Stopping_NGPT,LAYERS_Name.Остановка_НГПТ},
+        {LAYERS_Mat.mat_layer_Pedestrian_canopy,LAYERS_Name.Пешеходный_навес},
+        {LAYERS_Mat.mat_layer_Interceptive_parking,LAYERS_Name.Перехватывающий_паркинг},
+        {LAYERS_Mat.mat_layer_Reversal_playground,LAYERS_Name.Отстойно_D_разворотная_площадка},
+        {LAYERS_Mat.mat_layer_Elevated_pedestrian_crossing,LAYERS_Name.Надземный_пешеходный_переход},
+        {LAYERS_Mat.mat_layer_Station_NGPT,LAYERS_Name.Станция_НГПТ},
+        {LAYERS_Mat.mat_layer_Parking,LAYERS_Name.Парковки},
+        {LAYERS_Mat.mat_layer_Shopping_center,LAYERS_Name.Торговый_центр},
+        {LAYERS_Mat.mat_layer_Apartments,LAYERS_Name.Апартаменты},
+        {LAYERS_Mat.mat_layer_Offices,LAYERS_Name.Офисы},
+        {LAYERS_Mat.mat_layer_Residential_development,LAYERS_Name.Жилая_застойка},
+
+        {LAYERS_Mat.mat_layer_Athletic_wellness_complex,LAYERS_Name.Физкультурно_D_оздоровительный_комплекс},
+        {LAYERS_Mat.mat_layer_AZS,LAYERS_Name.АЗС},
+        {LAYERS_Mat.mat_layer_Entertaining_center,LAYERS_Name.Развлекательно_D_досуговый_центр},
+        {LAYERS_Mat.mat_layer_Cinema,LAYERS_Name.Кинотеатр},
+        {LAYERS_Mat.mat_layer_Showroom,LAYERS_Name.Выставочный_зал},
+        {LAYERS_Mat.mat_layer_Production_facility,LAYERS_Name.Производственный_объект},
+        {LAYERS_Mat.mat_layer_Kindergarten,LAYERS_Name.Десткий_сад},
+        {LAYERS_Mat.mat_layer_School,LAYERS_Name.Школа},
+        {LAYERS_Mat.mat_layer_Polyclinic,LAYERS_Name.Поликлиника},
+        {LAYERS_Mat.mat_layer_Social_object,LAYERS_Name.Социальный_объект},
+
+        {LAYERS_Mat.mat_layer_subway,LAYERS_Name.Метрополитен},
+        
+    };
+
+}
+
+[System.Serializable]
+public struct ScenObjectType
+{
+    public LAYERS_Name layerObject;
+    public Category CategoryObject;
+
+   
+    //public MeshRenderer[] objectAsignMaterial;
+    [Header("Пользовательское название слоя")]
+    [TextArea(3, 5)]
+    public string NameLayer;
+
+    /*[HideInInspector]
+    public Switch ThisSwith;*/
+    [HideInInspector]
+    public GameObject MeshShow;
+    private Material mat;
+    
+    
+
+     public void Inicialization()
+    {
+        if (NameLayer == String.Empty)
+        {
+            string nameLayer = layerObject.ToString();
+            NameLayer = nameLayer.Replace("_", " ");
+
+            if (NameLayer.Contains("D")) NameLayer = NameLayer.Replace("D", "-");
+        }
+
+        mat = Resources.Load<Material>(SettingLayer.LayerNames[layerObject]);
+    }
+}
+
+public class ObjectSetup : MonoBehaviour
+{
+    [HideInInspector]
+    public bool ExistPlaneLayer;
+    public int OrderOfPriority;
+    public ScenObjectType _scenObjectType;
+    [HideInInspector]
+    public MeshRenderer MeshRenderLayer;
+
+    [Space]
+    public MeshRenderer MeshRenderLayerPlane;
+    public MeshRenderer MeshRenderLayerUndeground;
+
+    [Space]
+    private ManagerToggle ManagerToggle;
+
+    public Color ColorLegend = Color.white; 
+
+    public virtual void Awake()
+    {
+        ManagerToggle = FindObjectOfType<ManagerToggle>();
+        _scenObjectType.MeshShow = gameObject;
+        _scenObjectType.Inicialization();
+        StartCoroutine(AddToManager());
+
+        foreach (Transform tr in transform)
+        {
+            if (tr.name.Contains("_plane"))
+            {
+                
+                MeshRenderLayerPlane = tr.GetComponent<MeshRenderer>();
+                if (MeshRenderLayerPlane)
+                {
+                    MeshRenderLayerPlane.enabled = false;
+                    ExistPlaneLayer = true;
+                }
+            }
+
+            if (tr.name.Contains("_undeground"))
+            {
+                MeshRenderLayerUndeground = tr.GetComponent<MeshRenderer>();
+                if (MeshRenderLayerUndeground) MeshRenderLayerUndeground.enabled = false;
+            }
+        }
+
+        if (!MeshRenderLayer&&GetComponent<MeshRenderer>()) MeshRenderLayer = GetComponent<MeshRenderer>();
+    }
+
+    private IEnumerator AddToManager()
+    {
+        yield return new WaitForSeconds(OrderOfPriority <= 0
+            ? (Time.deltaTime * transform.GetSiblingIndex() * 0.01f)
+            : (OrderOfPriority * 0.01f));
+
+        ManagerToggle.AddObject(this);
+        _scenObjectType.MeshShow.SetActive(false);
+    }
+    
+
+    public void ShowHideLayer()
+    {
+        
+    }
+    public void ChangeLayerViewType(bool isVoluneObject,bool isUndeground)
+    {
+        if (ExistPlaneLayer&& MeshRenderLayerPlane)
+        {
+            if (!MeshRenderLayerUndeground) MeshRenderLayerPlane.enabled = !isVoluneObject;
+
+            if (MeshRenderLayerUndeground)
+            {
+                MeshRenderLayerPlane.enabled = !isUndeground & !isVoluneObject;
+                MeshRenderLayerUndeground.enabled = isUndeground & !isVoluneObject;
+            }
+            //if (!isUndeground) MeshRenderLayerPlane.enabled = !isVoluneObject;
+//             else
+//             {
+                //if(MeshRenderLayerUndeground) 
+//            }
+
+            MeshRenderLayer.enabled = isVoluneObject;
+        }
+    }
+}
---
+++ TPU/Scripts/Win32Import.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+
+
+public enum SHOW_WINDOW
+{
+    /// <summary>
+    /// Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
+    /// </summary>
+    SW_FORCEMINIMIZE = 11,
+    /// <summary>
+    /// Hides the window and activates another window.
+    /// </summary>
+    SW_HIDE = 0,
+    /// <summary>
+    /// Maximizes the specified window.
+    /// </summary>
+    SW_MAXIMIZE = 3,
+
+    /// <summary>
+    /// Minimizes the specified window and activates the next top-level window in the Z order.
+    /// </summary>
+    SW_MINIMIZE = 6,
+
+    /// <summary>
+    /// Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
+    /// </summary>
+    SW_RESTORE = 9,
+
+    /// <summary>
+    /// Activates the window and displays it in its current size and position.
+    /// </summary>
+    SW_SHOW = 5,
+
+    /// <summary>
+    /// Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application.
+    /// </summary>
+    SW_SHOWDEFAULT = 10,
+
+    /// <summary>
+    /// Activates the window and displays it as a maximized window.
+    /// </summary>
+    SW_SHOWMAXIMIZED = 3,
+
+    /// <summary>
+    /// Activates the window and displays it as a minimized window.
+    /// </summary>
+    SW_SHOWMINIMIZED = 2,
+    /// <summary>
+    /// Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated.
+    /// </summary>
+    SW_SHOWMINNOACTIVE = 7,
+
+    /// <summary>
+    /// Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.
+    /// </summary>
+    SW_SHOWNA = 8,
+
+    /// <summary>
+    /// Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated.
+    /// </summary>
+    SW_SHOWNOACTIVATE = 4,
+    /// <summary>
+    /// Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+    /// </summary>
+    SW_SHOWNORMAL = 1
+    
+}
+
+public enum WINDOW_STYLES
+{
+    /// <summary>
+    /// The window has a thin-line border.
+    /// </summary>
+    WS_BORDER = 0x00800000,
+
+    /// <summary>
+    /// The window has a title bar (includes the WS_BORDER style).
+    /// </summary>
+    WS_CAPTION = 0x00C00000,
+
+    /// <summary>
+    /// The window is initially disabled. A disabled window cannot receive input from the user. To change this after a window has been created, use the EnableWindow function.
+    /// </summary>
+    WS_DISABLED = 0x08000000,
+
+    /// <summary>
+    /// The window has a border of a style typically used with dialog boxes. A window with this style cannot have a title bar.
+    /// </summary>
+    WS_DLGFRAME =  0x00400000,
+
+    /// <summary>
+    /// The window is initially maximized.
+    /// </summary>
+    WS_MAXIMIZE = 0x01000000,
+
+    /// <summary>
+    /// The window has a maximize button. Cannot be combined with the WS_EX_CONTEXTHELP style. The WS_SYSMENU style must also be specified.
+    /// </summary>
+    WS_MAXIMIZEBOX = 0x00010000,
+
+    /// <summary>
+    /// The window is initially minimized. Same as the WS_ICONIC style.
+    /// </summary>
+    WS_ICONIC  = 0x20000000,
+    WS_MINIMIZE = 0x20000000,
+
+    /// <summary>
+    /// The window has a minimize button. Cannot be combined with the WS_EX_CONTEXTHELP style. The WS_SYSMENU style must also be specified.
+    /// </summary>
+    WS_MINIMIZEBOX = 0x00020000,
+
+    /// <summary>
+    /// The window is an overlapped window. An overlapped window has a title bar and a border. Same as the WS_TILED style.
+    /// </summary>
+    WS_OVERLAPPED = 0x00000000,
+
+    /// <summary>
+    /// The window has a sizing border. Same as the WS_SIZEBOX style.
+    /// </summary>
+    WS_THICKFRAME = 0x00040000,
+
+    /// <summary>
+    /// The window has a window menu on its title bar. The WS_CAPTION style must also be specified.
+    /// </summary>
+    WS_SYSMENU = 0x00080000,
+
+    /// <summary>
+    /// The window is initially visible.
+    ///This style can be turned on and off by using the ShowWindow or SetWindowPos function.
+    /// </summary>
+    WS_VISIBLE  =0x10000000,
+
+
+    /// <summary>
+    /// WS_OVERLAPPEDWINDOW
+    ///(WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX)
+    /// </summary>
+
+
+
+
+
+    SW_HIDE = 0,
+    SW_NORMAL = 1,
+    SW_MINIMIZED = 2,
+    SW_MAXIMIZED = 3,
+    MAXIMIZE = 3,
+    SW_NORMAL_NO_ACTIVATE = 4,
+    SW_SHOW = 5,
+    MINIMIZE = 6,
+    SW_MIN_NO_ACTIVATE = 7,
+    SW_NO_ACTIVATE = 8,
+    SW_RESTORE = 9,
+    SW_DEFAULT = 10,
+    FORCE_MINIMIZED = 11
+};
+
+public enum WINDOWS_STYLE
+{
+    WS_BORDER         = 0x00800000,
+    WS_CAPTION        = 0x00C00000,
+    WS_CHILD          = 0x40000000,
+    WS_CHILDWINDOW    = 0x40000000,
+    WS_CLIPCHILDREN   = 0x02000000,
+    WS_CLIPSIBLINGS   = 0x04000000,
+    WS_DISABLED       = 0x08000000,
+    WS_DLGFRAME       = 0x00400000,
+    WS_MAXIMIZE       = 0x01000000,
+    WS_MAXIMIZEBOX    = 0x00010000,
+    WS_MINIMIZE       = 0x20000000,
+    WS_MINIMIZEBOX    = 0x00020000,
+    WS_OVERLAPPED     = 0x00000000,
+    WS_SIZEBOX        = 0x00040000,
+    WS_SYSMENU        = 0x00080000,
+    WS_VISIBLE        = 0x10000000
+}
+
+public enum WINDOW_POS
+{
+    /// <summary>
+    /// Places the window at the bottom of the Z order. If the hWnd parameter identifies a topmost window, the window loses its topmost status and is placed at the bottom of all other windows.
+    /// </summary>
+    HWND_BOTTOM = 1,
+    /// <summary>
+    /// Places the window above all non-topmost windows (that is, behind all topmost windows). This flag has no effect if the window is already a non-topmost window.
+    /// </summary>
+    HWND_NOTOPMOST = -2,
+    /// <summary>
+    /// Places the window at the top of the Z order.
+    /// </summary>
+    HWND_TOP = 0,
+    /// <summary>
+    /// Places the window above all non-topmost windows. The window maintains its topmost position even when it is deactivated.
+    /// </summary>
+    HWND_TOPMOST = -1,
+
+    /// <summary>
+    /// If the calling thread and the thread that owns the window are attached to different input queues, 
+    /// the system posts the request to the thread that owns the window. \
+    /// This prevents the calling thread from blocking its execution while other threads process the request.
+    /// </summary>
+    SWP_ASYNCWINDOWPOS = 0x4000,
+    /// <summary>
+    /// Retains the current Z order (ignores the hWndInsertAfter parameter).
+    /// </summary>
+    SWP_NOZORDER = 0x0004,
+    /// <summary>
+    /// Retains the current size (ignores the cx and cy parameters).
+    /// </summary>
+    SWP_NOSIZE = 0x0001,
+    /// <summary>
+    /// Displays the window.
+    /// </summary>
+    SWP_SHOWWINDOW = 0x0040,
+
+    /// <summary>
+    /// Retains the current position (ignores X and Y parameters).
+    /// </summary>
+    SWP_NOMOVE = 0x0002,
+
+    /// <summary>
+    /// Does not change the owner window's position in the Z order.
+    /// </summary>
+    SWP_NOREPOSITION = 0x0200,
+    /// <summary>
+    /// Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to the window, 
+    /// even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE is sent only when the window's size is being changed.
+    /// </summary>
+    SWP_FRAMECHANGED = 0x0020
+
+
+}
+
+public enum WINDOW_LONG
+{
+    /// <summary>
+    /// Sets a new extended window style.
+    /// </summary>
+    GWL_EXSTYLE = -20,
+    /// <summary>
+    /// Sets a new application instance handle.
+    /// </summary>
+    GWL_HINSTANCE = -6,
+    /// <summary>
+    /// Sets a new identifier of the child window.The window cannot be a top-level window.
+    /// </summary>
+    GWL_ID = -12,
+    /// <summary>
+    /// Sets a new window style.
+    /// </summary>
+    GWL_STYLE = -16,
+    /// <summary>
+    /// Sets the user data associated with the window. This data is intended for use by the application that created the window. Its value is initially zero.
+    /// </summary>
+    GWL_USERDATA = -21,
+    /// <summary>
+    /// Sets a new address for the window procedure. You cannot change this attribute if the window does not belong to the same process as the calling thread.
+    /// </summary>
+    GWL_WNDPROC = -4,          
+}
+
+public enum EXTENDED_WINDOW
+{
+    /// <summary>
+    /// The window is an overlapped window.
+    /// </summary>
+    WS_EX_WINDOWEDGE = 0x00000100,
+    /// <summary>
+    /// The window has a double border; the window can, optionally, be created with a title bar by specifying the WS_CAPTION style in the dwStyle parameter.
+    /// </summary>
+    WS_EX_DLGMODALFRAME = 0x00000001,
+    /// <summary>
+    /// The window should not be painted until siblings beneath the window (that were created by the same thread) have been painted. 
+    /// </summary>
+    WS_EX_TRANSPARENT = 0x20,
+    /// <summary>
+    /// The window is a layered window. This style cannot be used if the window has a class style of either CS_OWNDC or CS_CLASSDC.
+    /// </summary>
+    WS_EX_LAYERED = 0x80000      
+}
+
+public enum LAYERED_WINDOW_ATTRIBUTES
+{
+    /// <summary>
+    /// Use bAlpha to determine the opacity of the layered window.
+    /// </summary>
+    LWA_ALPHA = 0x00000002,
+    /// <summary>
+    /// Use crKey as the transparency color.
+    /// </summary>
+    LWA_COLORKEY = 0x00000001
+}
+
+public class Win32Import
+{
+    /*
+        public const int GWL_EXSTYLE = (-20);
+        public const int GWL_STYLE = (-16);
+        public const int WS_VISIBLE = 0x10000000;
+        public const int WS_CHILD = 0x40000000;
+        public const int WS_BORDER = 0x00800000;
+
+        public const int LWA_ALPHA = 0x00000002;
+        public const int WS_CAPTION = 0X00C0000;*/
+
+    
+    [DllImport("User32.dll", EntryPoint = "SendMessage")]
+    public static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll", EntryPoint = "SetParent")]
+    public static extern IntPtr SetParent(IntPtr child, IntPtr parent);
+
+    [DllImport("user32.dll", EntryPoint = "GetParent")]
+    public static extern IntPtr GetParent(IntPtr hWnd);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+    public extern static int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongA")]
+    public static extern int SetWindowLong(int hwnd, int nIndex, long dwNewLong);
+
+    [DllImport("user32.dll", EntryPoint = "MoveWindow")]
+    public static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
+
+    [DllImport("User32.dll", EntryPoint = "GetWindowLong")]
+    public static extern long GetWindowLong(int hwnd, int nIndex);
+    [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+    public static extern int GetWindowLong(IntPtr handle, int style);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    public static extern int SetWindowPos(int hwnd, int hwndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+    [DllImport("user32.dll")]
+    public static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+
+    [DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
+    public static extern int SetLayeredWindowAttributes(int hwnd, int crKey, byte bAlpha, int dwFlags);
+
+    
+
+    [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+    public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+    [DllImport("User32.dll")]
+    public static extern int SetLayeredWindowAttributes(IntPtr Handle, int crKey, byte bAlpha, int dwFlags);
+
+    [DllImport("user32.dll", EntryPoint = "SetFocus")]
+    public static extern IntPtr SetFocus(IntPtr hWnd);
+
+    /*   [DllImport("user32.dll", EntryPoint = "GetActiveWindow")]
+       public static extern int GetActiveWindow();*/
+
+    [DllImport("user32.dll", EntryPoint = "GetActiveWindow")]
+    public static extern System.IntPtr GetActiveWindow();
+
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindowAsync(int hWnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(string className, string windowTitle);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+
+
+
+
+
+}
