157740e53feac83dd519d0f89b667a98973a9f81
alla <anyway.amy@gmail.com>
2019-02-06 12:35:28 +0300

    8

---
+++ TPU/Editor/AfterBuild.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR_64||UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+#endif
+using UnityEngine;
+
+public class AfteBuild : MonoBehaviour {
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            Debug.Log(pathToBuiltProject);
+        }
+}
